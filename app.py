c8eeea8c6485cdb5d26016494e9af6ea6aad4dae
Hugo Talentino <htalentino@cscanada.ca>
2026-05-13 17:20:44 -0400

    correction train

--- app.py
+++ app.py
@@ -339,19 +339,24 @@ def show_rsvp_form():
     </div>
     """, unsafe_allow_html=True)
     
+    st.markdown('<div class="section-card">', unsafe_allow_html=True)
+
     st.markdown("""
-    <div style="
-        text-align:center;
-        margin-bottom:2rem;
-    ">
-    <h3>📍 Château de Bois Charmant, 15 Bois Charmant, 17380 Les Nouillers, France</h3>
+    <div class="section-title">
+    📍 Château de Bois Charmant, 15 Bois Charmant, 17380 Les Nouillers, France
     </div>
+
+    <p style="color:#7b6d6d;">
+    17380 Les Nouillers, France
+    </p>
     """, unsafe_allow_html=True)
 
     st.components.v1.iframe(
-        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2775.1292287916162!2d-0.6670321000000001!3d45.9287169!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4801214f9d7cf4c9%3A0x5bb861d2298e29b1!2sCh%C3%A2teau%20de%20bois%20charmant%20-%20Chambres%20d&#39;h%C3%B4tes!5e0!3m2!1sfr!2sca!4v1778706989901!5m2!1sfr!2sca",
-        height=400
+        "<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2775.1292287916162!2d-0.6670321000000001!3d45.9287169!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4801214f9d7cf4c9%3A0x5bb861d2298e29b1!2sCh%C3%A2teau%20de%20bois%20charmant%20-%20Chambres%20d&#39;h%C3%B4tes!5e0!3m2!1sfr!2sca!4v1778706989901!5m2!1sfr!2sca" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>",
+        height=450
     )
+
+    st.markdown('</div>', unsafe_allow_html=True)
 
     st.markdown("---")
     
